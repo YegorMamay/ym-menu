1dc71ebe7a8fae56e412ec92c34f58034b7ffd3c
YegorMamay <yegor19.05@gmail.com>
2015-12-20 20:21:54 +0200

    start

---
+++ scripts/menu.js
@@ -0,0 +1,18 @@
+$(document).ready(function () {
+    $("#menu-btn").click(function () {
+        if ($("nav").hasClass("menu-shift-right")) {
+                $("nav").removeClass("menu-shift-right");
+                $(".wrapper").animate({left: '0px'}, 300);
+                $(".curtain").animate({left: '0px'}, 200);
+        } else {
+            $("nav").addClass("menu-shift-right");
+            $(".wrapper").animate({left: '250px'}, 300);
+            $(".curtain").animate({left: '250px'}, 200);
+        }
+    });
+    $(".curtain").click(function () {
+        $("nav").removeClass("menu-shift-right");
+        $(".wrapper").animate({left: '0px'}, 300);
+        $(".curtain").animate({left: '100%'}, 200);
+    });
+});
